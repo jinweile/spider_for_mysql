7782a6b97a9a49840766c4357b59c1a683c0beb3
金为乐 <jinweile@163.com>
2014-08-07 22:23:18 +0800

    增加webservice----send

---
+++ src/main/java/com/etaoshi/spider/web/ISend.java
@@ -0,0 +1,5 @@
+package com.etaoshi.spider.web;
+
+public interface ISend {
+	public String call(String msg);
+}
---
+++ src/main/java/com/etaoshi/spider/web/Send.java
@@ -0,0 +1,10 @@
+package com.etaoshi.spider.web;
+
+public class Send implements ISend {
+
+	@Override
+	public String call(String msg) {
+		return msg;
+	}
+
+}
